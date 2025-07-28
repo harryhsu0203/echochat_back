b66a8d9b56bde7b20868a27c100fde5b5b2c1acb
harryhsu0203 <136421471+harryhsu0203@users.noreply.github.com>
2025-07-28 19:07:59 +0800

    Initial Commit

---
+++ echochat appTests/echochat_appTests.swift
@@ -0,0 +1,16 @@
+//
+//  echochat_appTests.swift
+//  echochat appTests
+//
+//  Created by 徐明漢 on 2025/7/28.
+//
+
+import Testing
+
+struct echochat_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
